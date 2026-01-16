017760223badede125c9d275bb4b31299c259bb8
Julio Pedro López Bayón <juliolb@educastur.org>
2026-01-16 17:10:40 +0100

    Actualización

---
+++ Habitantes y Municipios/Solución Consultas.sql
@@ -0,0 +1,118 @@
+-- 1. Nombre y teléfono de los habitantes de Luarca.
+
+	select nombre, teléfono 
+	from habitantes join vivienda on código_vivienda = código
+	where localidad = 'Luarca';
+
+
+-- 2. Nombre y teléfono de los habitantes del municipio de Valdés.
+
+	select habitantes.nombre, teléfono
+	from habitantes join vivienda on código_vivienda = vivienda.código
+		 join municipios on código_municipio = municipios.código
+	where municipios.nombre = 'Valdés';
+    
+
+-- 3. Dirección y metros cuadrados de las viviendas del municipio de Navia.
+
+	select dirección, cp, localidad, metros
+	from vivienda, municipios
+	where código_municipio = municipios.código and nombre = 'Navia';
+
+-- 4. Nombre y teléfono de aquellas personas que poseen una vivienda en Navia.
+
+	select distinct nombre, teléfono
+	from habitantes, vivienda, poseer
+	where código = poseer.código_vivienda && 
+		dni_habitante = dni &&
+		localidad = 'Navia';
+    
+	select distinct h.nombre, h.teléfono
+	from habitantes h, vivienda v, poseer p
+	where v.código = p.código_vivienda && 
+		p.dni_habitante = h.dni &&
+		v.localidad = 'Navia';  
+    
+	select distinct habitantes.nombre, habitantes.teléfono
+	from habitantes, vivienda, poseer
+	where vivienda.código = poseer.código_vivienda && 
+		poseer.dni_habitante = habitantes.dni &&
+		vivienda.localidad = 'Navia';
+    
+-- 5. De todas las viviendas del municipio de Avilés, su dirección,
+--    localidad y nombre del propietario.    
+
+-- 6. Nombre, dirección y teléfono de todos los cabeza de familia
+--    empadronados en el municipio de Tineo.
+
+-- 7. Número total de cabezas de familia empadronados en el
+--    municipio de Proaza.
+
+	select 	
+		count(distinct dni_cabeza_familia)	
+	from 
+		depender
+			join 
+		habitantes on dni_cabeza_familia = dni
+			join 
+		vivienda on código_vivienda = código
+			join
+		municipios on código_municipio = municipios. código
+	where 	
+		municipios.nombre = 'Proaza';
+
+
+-- 8. Número total de municipios en cada provincia junto con el nombre
+--    de la misma.
+
+
+
+
+-- 9. Cantidad total de personas a cargo de cada cabeza de familia de las
+--    localidades de Asturias cuyo nombre empieza o termina por la letra ‘s’.
+
+
+
+
+-- 10. Media de personas a cargo de un cabeza de familia en cada
+--     municipio de la provincia de Asturias.
+
+
+
+
+-- 11. Tamaño medio en metros cuadrados de las viviendas de cada
+--     municipio de la provincia de Asturias.
+
+	select nombre, avg(metros) 'promedio metros cuadrados'
+	from vivienda join municipios on código_municipio = municipios.código
+	where provincia = 'Asturias'
+	group by código_municipio, nombre;
+
+
+-- 12. Nombre, dirección y teléfono del cabeza de familia responsable
+--     de la persona con el D.N.I. 11.421.124.
+
+
+
+
+-- 13. Nombre y número de viviendas que posee cada cabeza de familia
+--     empadronado en un municipio de Asturias.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
