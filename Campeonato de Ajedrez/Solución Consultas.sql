4f49d131c100c05aabb6c2a31e3b1f43b426db69
Julio Pedro López Bayón <jplbayon@gmail.com>
2026-01-19 19:30:28 +0100

    Actualización

---
+++ Campeonato de Ajedrez/Solución Consultas.sql
@@ -0,0 +1,56 @@
+-- 1. Nombre, dirección y teléfono de todos los participantes de España.
+
+
+-- 2. Nombre y dirección de todos los hoteles en los que se han alojado jugadores de España.
+    
+
+-- 3. Nombre los participantes en cada partida junto con su nacionalidad y el número de partida.
+
+	
+-- 4. Obtener el nombre de los jugadores, el nombre del árbitro y el número de partida de las
+--    partidas celebradas el 10 de mayo de 2009, y también el nombre de hotel y de la sala donde
+--    se celebraron.
+
+
+-- 5. Nombre, dirección y teléfono del hotel u hoteles en los que se haya celebrado el mayor
+--    número de partidas.
+
+
+-- 6. Nombre de la sala o salas de mayor capacidad y nombre de su hotel.
+
+
+-- 7. Nombre, dirección y teléfono del hotel u hoteles que poseen la sala de mayor capacidad.
+
+
+-- 8. Obtener el nombre del hotel y sala donde se han celebrado partidas en las que el número de
+--    entradas vendidas ha sido inferior al 50% de la capacidad de la sala donde se han celebrado,
+--    y también el nombre de los jugadores y la fecha con el formato:
+--               <día_de_la_semana>, <día del més> de <año>
+
+
+-- 9. Obtener el número de partida, fecha de celebración, nombre de los participantes y número de
+--    movimientos de aquellas partidas en las que el número de movimientos realizados está entre
+--    los 5 mayores.
+
+
+-- 10. Nombre de todos los jugadores que no han participado en partidas celebradas un lunes.
+-- 11. Nombre de los jugadores cuya media de movimientos por partida supere a la media de
+--     movimientos por partida del resto de jugadores de su país.
+
+
+-- 12. Nombre de todas las salas junto al nombre de su hotel y el número de partidas celebradas en
+       cada una de ellas.
+
+
+-- 13. Nombre, dirección y teléfono del hotel u hoteles en los que se haya celebrado el mayor número
+--     de partidas.
+
+
+
+
+
+
+
+
+
+
