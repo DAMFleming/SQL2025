1ef560813c931d07b690afc702fbec1301d007f6
Julio Pedro López Bayón <juliolb@educastur.org>
2026-01-22 21:36:51 +0100

    Actualización

--- Campeonato de Ajedrez/Solución Consultas.sql
+++ Campeonato de Ajedrez/Solución Consultas.sql
@@ -14,7 +14,15 @@
 		
 
 -- 3. Nombre los participantes en cada partida junto con su nacionalidad y el número de partida.
-
+	select partidas.número 'Número de partida', prN.nombre 'J.Negras', paN.nombre 'País',
+			prB.nombre 'J.Blancas', paB.nombre 'País'
+	from paises paN join participantes prN on paN.número = prN.numPaís
+		join jugadores jN on prN.numAsociado = jN.numAsociado
+        join partidas on jN.numAsociado = partidas.negras
+        join jugadores jB on partidas.blancas = jB.numAsociado
+        join participantes prB on jB.numAsociado = prB.numAsociado
+        join paises paB on prB.numPaís = paB.número;
+	
 	
 -- 4. Obtener el nombre de los jugadores, el nombre del árbitro y el número de partida de las
 --    partidas celebradas el 10 de mayo de 2009, y también el nombre de hotel y de la sala donde
@@ -24,9 +32,20 @@
 -- 5. Nombre, dirección y teléfono del hotel u hoteles en los que se haya celebrado el mayor
 --    número de partidas.
 
+select nombreHotel
+from partidas
+group by nombreHotel
+having count(*) >= all (select distinct count(*)
+	                    from partidas
+                        group by nombreHotel);
 
 -- 6. Nombre de la sala o salas de mayor capacidad y nombre de su hotel.
-
+SELECT nombreSala, nombreHotel
+FROM salas
+WHERE capacidad = (
+	SELECT MAX(capacidad)
+	FROM salas
+);
 
 -- 7. Nombre, dirección y teléfono del hotel u hoteles que poseen la sala de mayor capacidad.
 
@@ -44,6 +63,12 @@
 
 -- 10. Nombre de todos los jugadores que no han participado en partidas celebradas un lunes.
 
+select nombre
+from participantes
+where numAsociado not in (
+	select distinct numAsociado
+	from jugadores join partidas on numAsociado = blancas || numAsociado = negras
+	where dayofweek(fecha) = 2);
 
 -- 11. Nombre de los jugadores cuya media de movimientos por partida supere a la media de
 --     movimientos por partida del resto de jugadores de su país.
@@ -62,6 +87,6 @@
 
 
 
-
+alter table partidas change entradas entradas integer;
 
 
