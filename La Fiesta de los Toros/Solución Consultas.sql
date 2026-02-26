1dfa655cf5e014e860d4eb72a248f4a8cf239e67
Julio Pedro López Bayón <juliolb@educastur.org>
2026-02-26 20:03:26 +0100

    Actualización

---
+++ La Fiesta de los Toros/Solución Consultas.sql
@@ -0,0 +1,65 @@
+-- 1. Lista de corridas celebradas entre los años 2007 y 2009 en la que figuren el año,
+--    la feria, el orden, la plaza y la localidad, en orden descendente por año, dentro
+--    de cada año en orden ascendente por feria y dentro de cada feria en orden ascendente
+--    por el atributo orden.
+
+
+
+-- 2. Nombre de las ferias que han celebrado alguna corrida en la Plaza de la Malagueta.
+
+
+
+-- 3. Nombre y localidad de las ganaderías que han proporcionado toros para las corridas
+--    celebradas en la Feria de Linares.
+
+
+
+-- 4. De las corridas celebradas antes del año 2005 en las que hayan participado toros de la
+--    ganadería Castillo de Azuel, mostrar el año de celebración, el nombre de la feria, y el
+--    nombre, dirección y localidad de la plaza donde se han celebrado.
+
+
+
+-- 5. Nombre de las plazas y número de corridas celebradas desde el año 2003 de aquellas plazas
+--    en las que se hayan celebrado más de tres corridas desde ese año.
+
+
+
+-- 6. Nombre de las plazas en las que se han celebrado corridas en el año 2007 y cantidad total
+--    de toreros distintos que han actuado en cada plaza.
+
+
+
+-- 7. Apodo del torero y nombre de su apoderado para todos aquellos toreros que hayan obtenido
+--    más de 50 premios en las corridas en las que han actuado durante el año 2009.
+
+
+
+-- 8. Nombre y apodo de cada torero junto con el número de corridas en las que ha actuado en los
+--    últimos 10 años.
+
+
+
+-- 9. Nombre y apodo, además del nombre y dirección de su apoderado, de todos aquellos toreros
+--    que nunca hayan toreado en la Feria de San Isidro.
+
+
+
+-- 10. Nombre y dirección de aquellos apoderados que apoderan toreros que han actuado en más de
+--     15 plazas.
+
+
+
+-- 11. Nombre y apodo de todos aquellos toreros que habiendo actuado en más de 500 corridas,
+--     nunca han toreado en la Plaza de las Ventas.
+
+
+
+-- 12. Número de ganaderías distintas que han proporcionado toros para las corridas en las que
+--     ha participado cada torero apadrinado por la persona cuyo D.N.I. es el 11222333.
+
+
+
+-- 13. Año y feria de aquellas corridas celebradas en la plaza con mayor aforo.
+
+
