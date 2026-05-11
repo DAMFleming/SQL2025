49f940305b38c04f8fe506b8dceeb9c4e39d0244
Julio Pedro López Bayón <juliolb@educastur.org>
2026-05-11 21:32:36 +0200

    Actualización

--- Triggers/Ejercicio2.sql
+++ Triggers/Ejercicio2.sql
@@ -1,4 +1,4 @@
-create table BuyPriceLogs (
+create table if not exists BuyPriceLogs (
 	productCode varchar(15),
 	updateDate datetime,
 	userName varchar(128),
@@ -10,6 +10,12 @@ create table BuyPriceLogs (
 delimiter //
 create trigger logbuyprice after update on classicmodels.products for each row
 begin
-	
+	insert into classicmodels.BuyPriceLogs
+	values (
+		old.productCode,
+		current_date(),
+		current_user(),
+		old.MSRP,
+		new.MSRP);
 end//
 delimiter ;
---
+++ Triggers/Ejercicio3.sql
@@ -0,0 +1,17 @@
+drop trigger if exists altaDetalle;
+delimiter //
+create trigger altaDetalle before insert on classicmodels.orderdetails for each row 
+begin
+	declare stock smallint;
+    declare vmsrp decimal(10, 2);
+    select quantityInStock, MSRP into stock, vmsrp from classicmodels.products where productCode = new.productCode;
+    if (stock < new.quantityOrdered) then
+		signal sqlstate '15000';
+    else
+		-- if (new.priceEach is null) then
+			set new.priceEach = vmsrp;
+		-- end if;
+		update classicmodels.products set quantityInStock = quantityInStock - new.quantityOrdered where productCode = new.productCode ;
+    end if;
+end//
+delimiter ;
