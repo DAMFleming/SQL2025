b8826aab8d5b90b43b125758f350b45bf697c347
Julio Pedro López Bayón <juliolb@educastur.org>
2026-05-11 21:36:21 +0200

    Actualización

--- Triggers/Ejercicio3.sql
+++ Triggers/Ejercicio3.sql
@@ -8,9 +8,9 @@ begin
     if (stock < new.quantityOrdered) then
 		signal sqlstate '15000';
     else
-		-- if (new.priceEach is null) then
+		if (new.priceEach is null) then
 			set new.priceEach = vmsrp;
-		-- end if;
+		end if;
 		update classicmodels.products set quantityInStock = quantityInStock - new.quantityOrdered where productCode = new.productCode ;
     end if;
 end//
--- Triggers/Prueba ejercicio 3.sql
+++ Triggers/Prueba ejercicio 3.sql
@@ -2,6 +2,7 @@ describe classicmodels.orderdetails;
 describe classicmodels.orders;
 describe classicmodels.products;
 select * from customers;
+select * from products;
 select * from products where productCode = 'S10_1678';
 -- 7930 -> 7927
 -- priceEach -> 95.70
@@ -10,5 +11,6 @@ select * from orders where orderNumber = 1234;
 select * from orderdetails where orderNumber = 1234;
 delete from orderdetails where orderNumber = 1234;
 
+
 insert into orders values (1234, current_date(), date_add(current_date(), interval 1 month), null, '*****', null, 103);
-insert into orderdetails values (1234, 'S10_1678', 3, 0, 1);  
+insert into orderdetails values (1234, 'S10_1949', 3, null, 2);  
